c40922a3146bd0825e360adfa54b87eb7146f924
amkkr <55781271+amkkr@users.noreply.github.com>
2023-12-23 18:29:35 +0900

    feat: stanのエラーを解消

--- src/app/Http/Controllers/ExampleController.php
+++ src/app/Http/Controllers/ExampleController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Laravel\Lumen\Http\ResponseFactory;
+
 class ExampleController extends Controller
 {
-    public function handle(int $number = 0)
+    public function handle(int $number = 0): JsonResponse
     {
-        return \response()
+        $response = new ResponseFactory();
+
+        return $response
             ->json([
                 'id' => 'aaa',
                 'name' => 'aadfas',
-                'number' => $number
-            ]);
+                'number' => $number,
+            ])
+        ;
     }
 }
